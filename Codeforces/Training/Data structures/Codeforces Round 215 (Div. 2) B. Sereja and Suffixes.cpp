167650f77ceb7e88679421e606677a1ed095dc0b
= <arsanyadel724@gmail.com>
2024-01-26 13:55:21 +0200

    Solved a 1100 data structures problem

---
+++ Codeforces/Training/Data structures/Codeforces Round 215 (Div. 2) B. Sereja and Suffixes.cpp
@@ -0,0 +1,47 @@
+// Problem Rate: 1100
+// Topics: data structures
+
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+#define endl "\n"
+#define pb push_back
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+void solve(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int n, m; cin >> n >> m;
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int count_distinct[n];
+    set<int> count;
+
+    for (int i = n - 1; i >= 0; i--){
+        count.insert(arr[i]);
+        count_distinct[i] = count.size();
+    }
+
+    while(m--){
+        int x; cin >> x;
+        cout << count_distinct[x - 1] << endl;
+    }
+}
+
+
+int main() {
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
